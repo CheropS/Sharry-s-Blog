c49f2ae77b5dc7083591cb035927441ab86c85b5
Sharon Cherop <sh.cherop@gmail.com>
2021-09-26 10:40:13 +0300

    adding email.py to authenticate email

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import ValidationError
 from wtforms import StringField,PasswordField,SubmitField, BooleanField, SubmitField
 from wtforms.validators import Required,Email,EqualTo
+from models import User
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,3 +1,9 @@
+from . import auth
+from ..models import User
+from .forms import LoginForm, RegistrationForm
+from .. import db
+from flask_login import login_user, logout_user, login_required
+from flask import render_template, redirect, url_for, flash, request
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -10,20 +16,8 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "watchlist login"
+    title = "blog login"
     return render_template('auth/login.html', login_form=login_form, title=title)
-
-# @auth.route('/register',methods = ["GET","POST"])
-# def register():
-#     form = RegistrationForm()
-
-#     if form.validate_on_submit():
-#         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         return redirect(url_for('auth.login'))
-#         title = "New Account"
-#     return render_template('auth/register.html',registration_form = form)
 
 @auth.route('/logout')
 @login_required
