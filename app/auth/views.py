3d6135f3df7e724ead7e6afaea490aa7042735af
Sharon Cherop <sh.cherop@gmail.com>
2021-09-28 04:50:49 +0300

    created database blog

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,9 @@ from .forms import LoginForm, RegistrationForm
 from .. import db
 from flask_login import login_user, logout_user, login_required
 from flask import render_template, redirect, url_for, flash, request
+from ..email import mail_message
+
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
