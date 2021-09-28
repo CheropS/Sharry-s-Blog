a0dada1c825427fe5cdce510065ec54fe10829f2
Sharon Cherop <sh.cherop@gmail.com>
2021-09-28 05:28:20 +0300

    user can now update their profiles

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from . import main
-from flask import render_template, abort
+from flask import render_template, abort, redirect, url_for
 from ..models import User
 from .. import db
+from .forms import UpdateProfile
+from flask_login import login_required
 
 
 @main.route('/')
@@ -28,3 +30,22 @@ def profife(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
