bd76952189ded4f3e9b53ff968fbbe287060832a
Sharon Cherop <sh.cherop@gmail.com>
2021-09-28 20:47:37 +0300

    changed to production

--- app/config.py
+++ app/config.py
@@ -32,20 +32,11 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DB")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URL")
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:password@kenyan/blog_test'
 
-class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    SQLALCHEMY_DATABASE_URI =os.environ.get('SQLALCHEMY_DATABASE_URI')
-    DEBUG = True
 
 config_options={
     'development': DevConfig, 
